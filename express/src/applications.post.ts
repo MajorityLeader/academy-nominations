6fcfa1d10647c3c592664bde03023cb0dab408fa
Shaun Brown <shaun.brown@mail.house.gov>
2022-08-02 18:54:51 +0100

    Add backend validators

--- express/src/applications.post.ts
+++ express/src/applications.post.ts
@@ -1,12 +1,28 @@
 import express, { Router, Request, Response } from "express";
-import { body, validationResult } from 'express-validator';
+import { validationResult, check } from 'express-validator';
 
 const router: Router = express.Router();
 router.post('/',
-    // username must be an email
-    body('username').isEmail(),
-    // password must be at least 5 chars long
-    body('password').isLength({ min: 5 }), async (req: Request, res: Response) => {
+    check('required-FIRSTNAME').exists().withMessage('first name is required'),
+    check('required-LASTNAME').exists().withMessage('last name is required'),
+    check('required-email').isEmail().withMessage('email is not valid'),
+    check('required-dob').isDate().withMessage('date of birth must be a date'),
+    check('required-pob').exists().withMessage('place of birth is required'),
+    check('required-street').exists().withMessage('street address is required'),
+    check('required-city').exists().withMessage('city is required'),
+    check('required-state').exists().withMessage('state is required'),
+    check('required-zipcode').exists().withMessage('postal code is required').isPostalCode('US').withMessage('not a valid US postal code'),
+    check('required-phone').isMobilePhone('any').withMessage('not a valid phone number'),
+    check('required-highschool-location').exists().withMessage('high school is required'),
+    check('required-highschool-graduation').exists().withMessage('high school year of graduation required').isNumeric().withMessage('high school graduation must be a number').isAfter('2018').withMessage('high school graduation should be after 2018'),
+    check('academy-selection-first').exists().withMessage('first choice of academy must be selected'),
+    check('academy-selection-second').exists().withMessage('second choice of academy must be selected'),
+    check('academy-selection-third').exists().withMessage('third choice of academy must be selected'),
+    check('required-contact-reference').exists().withMessage('letters of recommendation option must be chosen'),
+    check('file-transcript').exists().withMessage('transcript file must be attached'),
+    check('file-essay').exists().withMessage('essay file must be attached'),
+    check('file-photo').exists().withMessage('photo file must be attached'),
+    async (req: Request, res: Response) => {
     try {
         res.send('OK');
     } catch (e: any) {
