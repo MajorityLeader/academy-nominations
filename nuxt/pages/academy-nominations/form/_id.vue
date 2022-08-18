632ec9fd86f5d9db2bf0d898119cc937fabbf952
Shaun Brown <shaun.brown@mail.house.gov>
2022-08-18 14:07:28 +0200

    Init add components for form

---
+++ nuxt/pages/academy-nominations/form/_id.vue
@@ -0,0 +1,15 @@
+<script>
+export default ({
+  data() {
+    return {
+      form: {}
+    }
+  },
+  async $fetch() {
+    this.form = await this.$axios.$get(`/api/application/${this.$route.$params.query.a}`);
+    if (!form.firstName || !form.lastName || !form.birthday || !form.birthplace || !form.fatherName) {
+      this.$router.replace(`/academy-nominations/personal`)
+    }
+  }
+})
+</script>
