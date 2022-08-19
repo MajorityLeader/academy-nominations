e24c0269f873efdee47d926763a3c0d2cc8350aa
Shaun Brown <shaun.brown@mail.house.gov>
2022-08-19 13:40:19 +0200

    Update id to 32 bit

--- express/src/nanoid.ts
+++ express/src/nanoid.ts
@@ -1,6 +1,6 @@
 import { customAlphabet } from 'nanoid';
 import { alphanumeric } from 'nanoid-dictionary';
 
-const id = customAlphabet(alphanumeric, 11);
+const id = customAlphabet(alphanumeric, 32);
 
 export default id;
